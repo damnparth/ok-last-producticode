e2a69ee730242ebd17ea609c6267725229fda1b7
parth <dasparth1544@gmail.com>
2024-04-22 22:41:41 +0530

    added login auth

--- myapp/src/views/Login.vue
+++ myapp/src/views/Login.vue
@@ -1,14 +1,14 @@
 <template>
     <form>
         <h1> Login</h1>
-        <label for="">email</label>
-        <input type="email" required v-model="email">
-        <label for="">password</label>
-        <input type="password" requried v-model="password">
+        <label for="">username</label>
+        <input type="username" required v-model="username">
+        <label for="passText">password</label>
+        <input type="password" requried v-model="passText">
         <br>
         <br>
         <br>
-        <button class="button-login-signup"><router-link to="/" style="color: white; text-decoration: none;">submit</router-link></button>
+        <button class="button-login-signup" @click="validateForm">submit</button>
         <br>
         <br>
         <br>
@@ -20,14 +20,56 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     data(){
         return{
-            email:'',
-            password:''
+            username:'',
+            passText:''
 
         }
         
+    },
+    methods:
+    {
+        validateForm(){
+            let isValidated=true
+            const fields=[
+                {'username':this.username},
+                {'passText':this.passText}
+            ]
+            if(isValidated)
+            this.submitForm();
+
+
+        
+        },
+        submitForm(){
+            axios.post('http://127.0.0.1:8000/apis/login',{
+                username:this.username,
+                passText:this.passText
+            })
+            .then(response=>{
+                const user={
+                    'username':response.data.username,
+
+                }
+                setTimeout(()=>{this.$router.push('/')})
+            })
+
+
+
+        }
+
+
+
+
+
+
+
+
+
     }
 
 }
